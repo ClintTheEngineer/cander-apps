7d7f1e1abd2c2b82022741dbeaea974b280ddad4
ClintTheEngineer <reachme@clintalexander.dev>
2024-09-26 17:18:03 -0400

    url update 2

--- client/src/utils/Constants.jsx
+++ client/src/utils/Constants.jsx
@@ -1,8 +1,8 @@
 export const Constants = {
     SERVER_URL: (() => {
         const currentBaseUrl = window.location.hostname;
-        if (currentBaseUrl === 'localhost:5173') {
-            return new URL('http://localhost:8080');
+        if (currentBaseUrl === 'https://cander-apps.onrender.com') {
+            return new URL('https://cander-apps-server.onrender.com');
         } else {
             return new URL('https://cander-apps-server.onrender.com');
         }
