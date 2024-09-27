6e34b20f9dc6f0649f6cb5459ab429411241bb38
ClintTheEngineer <reachme@clintalexander.dev>
2024-09-27 12:44:51 -0400

    potential cors fix 4

--- server/index.js
+++ server/index.js
@@ -14,12 +14,15 @@ const clientDomain = process.env.CLIENT_DOMAIN;
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 
-app.use(bodyParser.json()); 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-app.use(cors({
-    origin: `${clientDomain}/login`, 
-    credentials: true 
-  }));
+
+app.use(cors())
+app.use(express.json())
 
 
 app.use((req, res, next) => {
